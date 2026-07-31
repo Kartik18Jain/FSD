9145ae4031b189181ea56d02e1ce11d3968a7b21
Kartik18Jain <kj0302279@gmail.com>
2026-07-31 09:49:33 +0530

    global scope

--- day_03/index.js
+++ day_03/index.js
@@ -1,13 +1,13 @@
-console.log(a);
-a = 45;
-var a ;
-a = 66;
-var a;
-{
+var a= 45;
+
+function f() {
   console.log(a);
-  var a = 12;
-  a = 65;
-  console.log(a);
-  var b = 66;
+ var a = 12;
+ let b = 12;
+ const c = 7;
+ console.log(b);
+ console.log(c);
 }
-console.log(b);
+console.log(f);
+f();
+console.log(a);
